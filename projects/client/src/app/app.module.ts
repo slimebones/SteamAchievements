360f58619b09f8fef49ae7db783e137472c225ad
ryzhovalex <thed4rkof@gmail.com>
2024-05-20 13:04:57 +0300

    update achievement.component.html, ...

--- projects/client/src/app/app.module.ts
+++ projects/client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NgxKitModule } from "@almazrpe/ngx-kit";
+import { GamesComponent } from './games/games.component';
+import { GameComponent } from './games/game/game.component';
+import { AchievementComponent } from './achievement/achievement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GamesComponent,
+    GameComponent,
+    AchievementComponent,
   ],
   imports: [
     BrowserModule,
---
+++ projects/client/src/app/games/games.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  templateUrl: './games.component.html',
+  styles: [
+  ]
+})
+export class GamesComponent {
+
+}
